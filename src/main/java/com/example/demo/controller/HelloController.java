0bf395c052c106bb8adb8542af42cefc70cb866c
Nickolay Grechkin <nickolay.grechkin@binary-studio.com>
2022-12-03 08:09:58 +0200

    added main page

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,11 +1,12 @@
-package com.example.demo;
+package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
     @GetMapping("/")
