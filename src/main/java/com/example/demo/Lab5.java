0fcb237a5a3ee3092cb7ca75fd2de565204509ad
Daria Kostuk <dkostuk09@gmail.com>
2022-12-03 10:49:54 +0200

    Added repository

---
+++ src/main/java/com/example/demo/Lab5.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Collections;
+
+@SpringBootApplication
+public class Lab5 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab5.class, args);
+    }
+
+}
--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -3,11 +3,9 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
-public class HelloController {
+public class MainController {
 
     @GetMapping("/")
     public String emptyRequest() {
